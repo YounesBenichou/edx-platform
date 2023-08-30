c2b1714dc94867a2a4cb45508d0d2989038e694e
Seifoo <is_hafri@esi.dz>
2023-08-30 21:32:44 +0100

    badge model in progress before pulling

--- gamification/admin.py
+++ gamification/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Badge
 
 # Register your models here.
+
+
+@admin.register(Badge)
+class BadgeAdmin(admin.ModelAdmin):
+    list_display = ("name", "rule", "created", "modified")
+    search_fields = ("name",)
+    list_filter = ("created", "modified")
--- gamification/models.py
+++ gamification/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# Badge Model
+
+
+class Badge(models.Model):
+    badge_image = models.ImageField(upload_to="badge_images/")
+    name = models.CharField(max_length=255)
+    rule = models.PositiveIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
