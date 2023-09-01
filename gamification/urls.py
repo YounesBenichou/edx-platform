2c6e8e938a2cdc2ac7c1b3485e44eeaa08397d46
Seifoo <is_hafri@esi.dz>
2023-09-01 22:44:30 +0100

    handle_award  tested

--- gamification/urls.py
+++ gamification/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    # gamification parameters
+    # gamification parameters (how much  points we get for completing a unit  - sectiom - course)
     path(
         "v1/gamification_parameters/",
         views.get_gamification_parameters,
@@ -15,7 +15,7 @@ urlpatterns = [
         views.modify_gamification_parameters,
         name="modify-gamification_parameters",
     ),
-    # Score
+    # Score of user
     path("v1/score_user/", views.get_score_all, name="get-score_user_all"),
     path("v1/score_user/<int:user_id>/", views.get_score_user, name="get-score_user"),
     path(
@@ -23,17 +23,17 @@ urlpatterns = [
         views.modify_score_user,
         name="modify-score_user",
     ),
-    # Badge
+    # Badges
     path("v1/badges/", views.get_all_badges, name="get_all_badges"),
     path("v1/badges/modify/<int:badge_id>/", views.modify_badge, name="modify_badge"),
     path("v1/badges/create/", views.create_badge, name="create_badge"),
-    # modify score
+    # modify score (this overrides the user's score)
     path(
         "v1/modify_score/<int:user_id>/<int:new_score>/",
         views.modify_score,
         name="modify_score",
     ),
-    # updatescore
+    # updatescore when unit - section - course completed, it adds the parameter to the old  score
     path(
         "v1/update_score/<int:user_id>/<str:type>/",
         views.update_score,
@@ -50,7 +50,8 @@ urlpatterns = [
         views.user_award_update,
         name="user_award_update",
     ),
-    # handleaward
+    # this one should be called when a user(user_id) clicks convert on a certain award(award_id) and it handles how awards are given it return
+    # it return award granted for user or points isnufficient
     path(
         "v1/handle_award/<int:user_id>/<int:award_id>/",
         views.handle_award,
